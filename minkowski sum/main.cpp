61f1bc0af844fbd785a94ddf8eb41800ee85afef
hsahni55h <h.sahni1998@gmail.com>
2023-09-15 12:26:04 +0200

    minkowski sum working, with all the extra things commented out

--- minkowski sum/main.cpp
+++ minkowski sum/main.cpp
@@ -8,9 +8,9 @@ using namespace std;
 void print2DVector(vector<vector<int> > vec)
 {
      // Sorting the vector for grading purpose
-    sort(vec.begin(), vec.end());
-    for (int i = 0; i < vec.size(); ++i) {
-        for (int j = 0; j < vec[0].size(); ++j) {
+    // sort(vec.begin(), vec.end());
+    for (size_t i = 0; i < vec.size(); ++i) {
+        for (size_t j = 0; j < vec[0].size(); ++j) {
                 cout << vec[i][j] << "  ";
         }
         cout << endl;
@@ -18,37 +18,42 @@ void print2DVector(vector<vector<int> > vec)
 }
 
 // ***TODO: Check for duplicate coordinates inside a 2D vector and delete them*** //
-vector<vector<int> > delete_duplicate(vector<vector<int> > C)
-{
+// vector<vector<int> > delete_duplicate(vector<vector<int> > C)
+// {
 
     
     
     
     
     
-}
+// }
 
 // ***TODO: Compute the Minkowski Sum of two vectors***//
 vector<vector<int> > minkowski_sum(vector<vector<int> > A, vector<vector<int> > B)
 {
-    
-    
-    
-    
-    
-    
-    
-    C = delete_duplicate(C);
-    return C;
+    vector<vector <int> > result;
+
+    for(size_t i = 0; i < A.size(); i++)
+    {
+        for (size_t j = 0; j < B.size(); j++)
+        {
+            vector<int> sum = {A[i][0] + B[j][0], A[i][1] + B[j][1]};
+            result.push_back(sum);
+        }
+    }
+
+    // C = delete_duplicate(C);
+    return result;
 }
 
 int main()
 {
     // ***TODO: Define the coordinates of triangle A and B using 2D vectors*** //
-    
+    vector<vector<int>> A = {{1,0}, {0,1}, {0,-1}};
+    vector<vector<int>> B = {{0,0}, {1,1}, {1,-1}};
     
     // Compute the minkowski sum of triangle A and B
-    vector<vector<int> > C;
+    vector<vector<int>> C;
     C = minkowski_sum(A, B);
 
     // Print the resulting vector
