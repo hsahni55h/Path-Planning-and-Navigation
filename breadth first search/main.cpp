57647a8355d9687c54c95dcad9761f3ccaf2a634
hsahni55h <h.sahni1998@gmail.com>
2023-09-22 12:26:25 +0200

    commit

--- breadth first search/main.cpp
+++ breadth first search/main.cpp
@@ -35,71 +35,114 @@ The Cost of Movement value indicates how much it cost to move from one cell to a
 
 using namespace std;
 
-/* TODO: Define a Map class
-   Inside the map class, define the mapWidth, mapHeight and grid as a 2D vector
-*/
+// Map class
 class Map {
 public:
     const static int mapWidth = 6;
     const static int mapHeight = 5;
-    vector <vector<int>> grid = {
-        {0,1,0,0,0,0},
-        {0,1,0,0,0,0},
-        {0,1,0,0,0,0},
-        {0,1,0,0,0,0},
-        {0,0,0,1,1,0},
-    }; 
-};
-
-/* TODO: Define a Planner class
-   Inside the Planner class, define the start, goal, cost, movements, and movements_arrows
-   Note: The goal should be defined it terms of the mapWidth and mapHeight
-*/
-
-class Planner: Map{
-public:
-    int start[2] = {0,0};
-    int goal[2] = {mapHeight -1, mapWidth -1};
-    int cost = 1;
-
-    string movements_arrows[4] = {"^", "<", "v", ">"};
-    vector<vector<int>> movements{
-        {-1, 0},
-        {0, -1},
-        {1, 0},
-        {0, 1}
+    vector<vector<int> > grid = {                   // 0 indicates empty cell and 1 indicates obstacle
+        { 0, 1, 0, 0, 0, 0 },
+        { 0, 1, 0, 0, 0, 0 },
+        { 0, 1, 0, 0, 0, 0 },
+        { 0, 1, 0, 0, 0, 0 },
+        { 0, 0, 0, 1, 1, 0 }
     };
 };
 
-/* TODO: Define a print2DVector function which will print 2D vectors of any data type
-   Example
-   
-   Input: 
-   vector<vector<int> > a{{ 1, 0 },{ 0, 1 }};
-   print2DVector(a);
-   vector<vector<string> > b{{ "a", "b" },{ "c", "d" }};
-   print2DVector(b);
-   
-   Output:
-   1 0
-   0 1
-   a b
-   c d
-   Hint: You need to use templates
-*/
+// Planner class
+class Planner : Map {
+public:
+    int start[2] = { 0, 0 };
+    int goal[2] = { mapHeight - 1, mapWidth - 1 };
+    int cost = 1;
 
-template<typename T>
-void print2DVector(const vector<vector<T>>& vec) {
-    for (size_t i = 0; i < vec.size(); ++i) {
-        for (size_t j = 0; j < vec[i].size(); ++j) {
-            cout << vec[i][j] << "  ";
+    string movements_arrows[4] = { "^", "<", "v", ">" };
+
+    vector<vector<int> > movements{
+        { -1, 0 },
+        { 0, -1 },
+        { 1, 0 },
+        { 0, 1 }
+    };
+};
+
+// Template function to print 2D vectors of any type
+template <typename T>
+void print2DVector(T Vec)
+{
+    for (int i = 0; i < Vec.size(); ++i) {
+        for (int j = 0; j < Vec[0].size(); ++j) {
+            cout << Vec[i][j] << ' ';
         }
         cout << endl;
     }
 }
 
+/*#### TODO: Code the search function which will generate the expansion list ####*/
+// You are only required to print the final triplet values
+void search(Map map, Planner planner)
+{
+    vector<vector<int>> closed(map.mapHeight, vector<int> (map.mapWidth));
+    closed[planner.start[0]][planner.start[1]] = 1;                             // marking the visited nodes
 
-/*############ Don't modify the main function############*/
+    size_t x = planner.start[0];
+    size_t y = planner.start[1];
+    int g = 0;
+
+    vector<vector<int>> open;
+    open.push_back({g, static_cast<int>(x), static_cast<int>(y)});
+
+    bool found = false;
+    bool empty = false;
+
+    size_t x2;
+    size_t y2;
+
+    while(!found && !empty)
+    {
+        if (open.size() == 0)
+        {
+            empty = true;
+            cout << "Failed to reach a goal" << endl;
+        }
+
+        else
+        {
+            sort(open.begin(), open.end());
+            reverse(open.begin(), open.end());
+
+            vector <int> next;
+            x = static_cast<size_t>(next[1]);
+            y = static_cast<size_t>(next[2]);
+            g = next[0];
+            
+            if (x == static_cast<size_t>(planner.goal[0]) && y == static_cast<size_t>(planner.goal[1]))
+            {
+                found = true;
+                cout << "[" << g << ", " << x << ", " << y << "]" << endl;
+            }
+
+            else
+            {
+                for(size_t i = 0; i < planner.movements.size(); i++)
+                {
+                    x2 = x + planner.movements[i][0];
+                    y2 = y + planner.movements[i][1];
+                    if (x2 >= 0 && x2 < map.grid.size() && y2 >= 0 && y2 < map.grid[0].size())
+                    {
+                        if (closed[x2][y2] == 0 and map.grid[x2][y2] == 0) 
+                        {
+                            int g2 = g + planner.cost;
+                            open.push_back({ g2, static_cast<int>(x2), static_cast<int>(y2)});
+                            closed[x2][y2] = 1;
+                        };
+                    };
+                };
+
+            };
+        };
+    };
+};
 
 int main()
 {
@@ -107,15 +150,8 @@ int main()
     Map map;
     Planner planner;
 
-    // Print classes variables
-    cout << "Map:" << endl;
-    print2DVector(map.grid);
-    cout << "Start: " << planner.start[0] << " , " << planner.start[1] << endl;
-    cout << "Goal: " << planner.goal[0] << " , " << planner.goal[1] << endl;
-    cout << "Cost: " << planner.cost << endl;
-    cout << "Robot Movements: " << planner.movements_arrows[0] << " , " << planner.movements_arrows[1] << " , " << planner.movements_arrows[2] << " , " << planner.movements_arrows[3] << endl;
-    cout << "Delta:" << endl;
-    print2DVector(planner.movements);
+    // Search for the expansions
+    search(map, planner);
 
     return 0;
 }
